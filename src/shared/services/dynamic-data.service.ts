7a82b79579a40565e3fb584e5fcf9ec65504f742
manhnd <manhnd@smartgift.vn>
2026-02-04 09:59:32 +0700

    refactor

---
+++ src/shared/services/dynamic-data.service.ts
@@ -0,0 +1,41 @@
+import { Role } from '@authentication/enums/role.enum';
+import { Injectable } from '@nestjs/common';
+import { SystemLabel, SystemLabelLang } from '@shared/enums/system-label.enum';
+import { SettingService } from '@shared/setting/services/setting.service';
+
+const EnumRegistry = {
+  shared: {
+    systemLabel: { SystemLabel, SystemLabelLang },
+  },
+  authentication: {
+    role: { Role },
+  },
+};
+
+@Injectable()
+export class DynamicDataService {
+  constructor(private readonly settingService: SettingService) {}
+
+  async getSettings(keys: string[]): Promise<Record<string, any>> {
+    const settings = await Promise.all(
+      keys.map(async (key) => [key, await this.settingService.get(key)])
+    );
+
+    return Object.fromEntries(settings);
+  }
+
+  async getEnums(enumPaths: string[]): Promise<Record<string, any>> {
+    const result: Record<string, any> = {};
+
+    for (const enumPath of enumPaths) {
+      try {
+        const [modulePath, fileName] = enumPath.split('.');
+        result[enumPath] = EnumRegistry[modulePath]?.[fileName] || null;
+      } catch (error) {
+        result[enumPath] = null;
+      }
+    }
+
+    return result;
+  }
+}

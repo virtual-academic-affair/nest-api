cf3b6bcfc512ca9fafa276e72dbe64c3d6cd43c4
manhnd <manhnd@smartgift.vn>
2026-02-27 14:33:24 +0700

    refactor

--- src/class-registration/dtos/class-registrations/update.dto.ts
+++ src/class-registration/dtos/class-registrations/update.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType, OmitType } from '@nestjs/swagger';
 import { CreateDto } from './create.dto';
 
-export class UpdateDto extends PartialType(OmitType(CreateDto, ['messageId'] as const)) {}
+export class UpdateDto extends PartialType(OmitType(CreateDto, ['messageId', 'items'] as const)) {}

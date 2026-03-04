7cdd5e32491e1f174937ee989b37614cb97df77c
manhnd <manhnd@smartgift.vn>
2026-03-04 09:21:10 +0700

    refactor

--- src/inquiry/enums/inquiry-type.enum.ts
+++ src/inquiry/enums/inquiry-type.enum.ts
@@ -1,5 +1,5 @@
 export enum InquiryType {
-  Graduation = 'tot_nghiep',
-  Process = 'qua_trinh',
-  Procedure = 'don_tu_thu_tuc',
+  Graduation = 'graduation',
+  Process = 'process',
+  Procedure = 'procedure',
 }

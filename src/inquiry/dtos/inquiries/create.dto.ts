c021bfede1e102be1e291e35a255006e1e061407
manhnd <manhnd@smartgift.vn>
2026-03-04 08:45:38 +0700

    refactor

--- src/inquiry/dtos/inquiries/create.dto.ts
+++ src/inquiry/dtos/inquiries/create.dto.ts
@@ -1,8 +1,13 @@
-import { IsString, MaxLength } from 'class-validator';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 import { HasMessageIdDto } from '@email/dtos/messages/has-message-id.dto';
 
 export class CreateDto extends HasMessageIdDto {
   @IsString()
   @MaxLength(5000)
   question!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(5000)
+  answer?: string;
 }
--- src/inquiry/dtos/inquiries/update.dto.ts
+++ src/inquiry/dtos/inquiries/update.dto.ts
@@ -1,10 +1,4 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
-import { IsOptional, IsString, MaxLength } from 'class-validator';
 import { CreateDto } from './create.dto';
 
-export class UpdateDto extends PartialType(OmitType(CreateDto, ['messageId'] as const)) {
-  @IsOptional()
-  @IsString()
-  @MaxLength(5000)
-  answer?: string;
-}
+export class UpdateDto extends PartialType(OmitType(CreateDto, ['messageId'] as const)) {}

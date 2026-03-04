90872cb12f8611c05c7b4c9bdd4534f85b50f2a2
manhnd <manhnd@smartgift.vn>
2026-03-04 09:46:02 +0700

    refactor

--- src/app/http/setup.ts
+++ src/app/http/setup.ts
@@ -5,6 +5,7 @@ import { HttpAppModule } from './http-app.module';
 export async function setupHttpApp(port?: number): Promise<{ app: INestApplication; port: number; url: string }> {
   const app = await NestFactory.create(HttpAppModule);
   app.enableCors();
+  app.getHttpAdapter().getInstance().set('query parser', 'extended');
 
   const HTTP_PORT = port || Number(process.env.HTTP_PORT) || 3000;
   const url = `http://localhost:${HTTP_PORT}`;

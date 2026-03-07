350104428e64f517726f6072b57a1a921af1e524
manhnd <manhnd@smartgift.vn>
2026-03-07 14:29:57 +0700

    refactor

--- src/email/controllers/messages.controller.ts
+++ src/email/controllers/messages.controller.ts
@@ -1,15 +1,15 @@
-import { Body, Controller, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { Auth } from '@authentication/decorators/auth.decorator';
 import { Roles } from '@authentication/decorators/roles.decorator';
 import { AuthType } from '@authentication/enums/auth-type.enum';
 import { Role } from '@authentication/enums/role.enum';
 import { QueryDto } from '@email/dtos/messages/query.dto';
+import { UpdateLabelsDto } from '@email/dtos/messages/update-labels.dto';
 import { Message } from '@email/entities/message.entity';
 import { EmailSyncService } from '@email/services/email-send/email-sync.service';
 import { MessageLabelsService } from '@email/services/message-labels.service';
 import { MessagesService } from '@email/services/messages.service';
-import { SystemLabel } from '@shared/enums/system-label.enum';
 import { ResourceController } from '@shared/resource/controllers/resource.controller';
 import { RestrictMethods } from '@shared/resource/decorators/restrict-methods.decorator';
 import { ResourceAction } from '@shared/resource/enums/resource-action.enum';
@@ -40,10 +40,7 @@ export class MessagesController extends ResourceController<Message> {
 
   @Put(':id/labels')
   @GrpcMethod('MessageService', 'UpdateLabels')
-  async updateLabels(
-    @Body() data: { messageId: number; systemLabels: SystemLabel[] },
-    @Param('id', ParseIntPipe) messageId?: number,
-  ) {
+  async updateLabels(@Body() data: UpdateLabelsDto, @Param('id', ParseIntPipe) messageId?: number) {
     return await this.messageLabelsService.run(messageId || data.messageId, data.systemLabels);
   }
 }
---
+++ src/email/dtos/messages/update-labels.dto.ts
@@ -0,0 +1,16 @@
+import { Type } from 'class-transformer';
+import { ArrayUnique, IsArray, IsEnum, IsInt, IsOptional, Min } from 'class-validator';
+import { SystemLabel } from '@shared/enums/system-label.enum';
+
+export class UpdateLabelsDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  messageId?: number;
+
+  @IsArray()
+  @ArrayUnique()
+  @IsEnum(SystemLabel, { each: true })
+  systemLabels!: SystemLabel[];
+}

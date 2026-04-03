1308644d999095f3d46765f1de4687c046de1ed7
manhnd <manhnd@smartgift.vn>
2026-04-04 01:18:28 +0700

    inquiry

--- src/inquiry/enums/inquiry-type.enum.ts
+++ src/inquiry/enums/inquiry-type.enum.ts
@@ -5,7 +5,7 @@ export enum InquiryType {
 }
 
 export const InquiryTypeLang: Record<InquiryType, Record<string, string>> = {
-  [InquiryType.Graduation]: { vi: 'Tốt nghiệp', en: 'Graduation', color: '#a479e2' },
-  [InquiryType.Training]: { vi: 'Đào tạo', en: 'Training', color: '#2da2bb' },
-  [InquiryType.Procedure]: { vi: 'Thủ tục', en: 'Procedure', color: '#fad165' },
+  [InquiryType.Graduation]: { vi: 'Thắc mắc/Tốt nghiệp', en: 'Graduation', color: '#a479e2' },
+  [InquiryType.Training]: { vi: 'Thắc mắc/Đào tạo', en: 'Training', color: '#2da2bb' },
+  [InquiryType.Procedure]: { vi: 'Thắc mắc/Thủ tục', en: 'Procedure', color: '#fad165' },
 };
